dc3ad2b9d522dd5e12ba97e7c2a83608d10ad1ff
Vinisha Gupta <vinishasgupta@gmail.com>
2026-06-22 18:06:39 +0530

    1.to exe2

--- 1. to/script.js
+++ 1. to/script.js
@@ -11,9 +11,25 @@
 // Make box move 600px right in 3 seconds.
 
 
-gsap.to(".box",
-    {
-        duration:3,
-        x:600
-    }
-)
+// gsap.to(".box",
+//     {
+//         duration:3,
+//         x:600
+//     }
+// )
+
+
+// Exercise 2 — Move diagonally ↘️
+
+// Move:
+
+// right 400px
+// down 300px
+
+
+// gsap.to(".box",
+//    { x:400,
+//     y:300}
+// )
+
+
